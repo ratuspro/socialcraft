371fa63e169cc33be1aa643516b200ad0ae7051d
Diogo Rato <diogo.rato.11@gmail.com>
2021-10-28 16:20:32 +0000

    Add docker url for AgentManager (#7)

--- src/core/agent_manager.py
+++ src/core/agent_manager.py
@@ -12,12 +12,17 @@ from core.agent import Agent
 class AgentManager:
     '''
     The AgentManager class supervises the deployment of agents
+    
     '''
-    def __init__(self):
+    def __init__(self, docker_url=None):
         self.__cache = AgentCache()
+        if docker_url is not None:
+            self.__docker_client = docker.DockerClient(base_url=docker_url)
+        else:
+            self.__docker_client = docker.from_env()
 
     def __get_docker_client(self) -> Optional[DockerClient]:
-        return docker.from_env()
+        return self.__docker_client
 
     def __get_docker_container(self, name: str) -> Optional[Container]:
         try:
