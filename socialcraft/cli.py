d2d9d94dca27ac6b1ab673264238f6af07455944
Diogo Rato <diogo.rato.11@gmail.com>
2021-10-29 10:20:00 +0100

    Add module entrypoint

--- socialcraft/cli.py
+++ socialcraft/cli.py
@@ -2,11 +2,8 @@
 SocialCraft Command Line Interface
 """
 import typer
-import commands
+from socialcraft import commands
 
 app = typer.Typer()
 
 app.add_typer(commands.cli, name="agent")
-
-if __name__ == '__main__':
-    app()
