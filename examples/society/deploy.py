c584855684cc51ace6f0ce659843098c8621dbe8
ratuspro <diogo.rato.11@gmail.com>
2022-01-16 13:32:03 +0100

    Add one agent using python

---
+++ examples/society/deploy.py
@@ -0,0 +1,24 @@
+from socialcraft import AgentManager
+import pathlib
+import os
+import random
+
+if __name__ == "__main__":
+
+    # Connect to Agent Manager
+    manager = AgentManager(minecraft_host="host.docker.internal", minecraft_port=25565)
+
+    # Create Agent Blueprint
+    path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)), "blueprint/")
+
+    agent_blueprint = manager.generate_blueprint(
+        name="social_bot", agent_source_path=str(path)
+    )
+
+    all_agents = manager.get_all_agents()
+
+    for old_agent in all_agents:
+        old_agent.kill()
+
+    agent = manager.create_agent("Joe", blueprint=agent_blueprint)
+    agent.deploy()
