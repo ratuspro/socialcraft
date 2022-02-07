6af8ad86734aa00f15f4332de4c6a52fba4ace4b
ratuspro <diogo.rato.11@gmail.com>
2022-02-07 10:32:10 +0100

    Add state to practices

--- examples/builder/blueprint/csf/practices/practice.py
+++ examples/builder/blueprint/csf/practices/practice.py
@@ -1,14 +1,30 @@
 from abc import abstractmethod, ABC
 import logging
+from sre_parse import State
 import sys
+import enum
 
 
 class Practice(ABC):
+    class State(enum.Enum):
+        CREATED = 1
+        RUNNING = 2
+        STOPPED = 3
+        FINISHED = 4
+
     def __init__(self, creator) -> None:
         self._creator = creator
+        self._state = Practice.State.CREATED
+
+    @property
+    def state(self):
+        return self._state
 
     @abstractmethod
     def start(self) -> None:
+        pass
+
+    def update(self) -> None:
         pass
 
     @abstractmethod
